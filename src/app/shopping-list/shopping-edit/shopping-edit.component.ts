b688377d9b36121d2cb5b45ebf02c63237c64a10
zurab sudadze <z.sudadze@court.gov.ge>
2019-12-23 05:33:17 -0800

    bindings again

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -19,6 +19,5 @@ export class ShoppingEditComponent implements OnInit {
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
     this.ingredientAdded.emit(newIngredient);
-    console.log(newIngredient.name);
   }
 }
