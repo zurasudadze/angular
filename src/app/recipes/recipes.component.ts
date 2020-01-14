0b5a82071aabdf2a6e71ba2b87e25c4d19508039
zurab sudadze <z.sudadze@court.gov.ge>
2020-01-14 15:38:18 +0400

    recipes changed

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -14,11 +14,11 @@ export class RecipesComponent implements OnInit {
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
+  /*  this.recipeService.recipeSelected.subscribe(
       (recipe: Recipe) => {
         this.selectedRecipe = recipe;
       }
-    );
+    );*/
   }
 
 }
